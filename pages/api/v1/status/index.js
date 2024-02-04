bc54b403faeb7cbd22c50fea1cdbdef3d4be2897
Luis Gustavo Monteiro <lgmluisgm@gmail.com>
2024-02-04 19:09:49 +0000

    add `.env` and `database.js` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(req, res) {
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   res.status(200).json({ bão: "demais" });
 }
 
