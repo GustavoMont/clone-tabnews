29a427a70c46399ee4519227c602c48c5fb42908
Luis Gustavo Monteiro <lgmluisgm@gmail.com>
2024-02-06 03:36:19 +0000

    make `database.js` more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -8,10 +8,14 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
