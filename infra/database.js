5c101a26119b0f60e0d881998879d369d0112010
Luis Gustavo Monteiro <lgmluisgm@gmail.com>
2024-02-27 22:42:41 +0000

    make `dattabase.js` more robust to connection errors

--- infra/database.js
+++ infra/database.js
@@ -13,6 +13,8 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
+    console.log(error);
+    throw error;
   } finally {
     await client.end();
   }
