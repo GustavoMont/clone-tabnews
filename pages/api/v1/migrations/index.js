787fdfac22fb072b65290c17dd6fb233d7d95a35
GustavoMont <lgmluisgm@gmail.com>
2024-04-27 12:21:40 -0300

    fixes not allowed methods bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,29 +3,38 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(req, res) {
-  const dbClient = await database.getNewClient();
-  const defaultMigrationOptions = {
-    dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-  if (req.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    await dbClient.end();
-    return res.status(200).json(pendingMigrations);
+  const allowedMethos = ["GET", "POST"];
+  if (!allowedMethos.includes(req.method)) {
+    return res.status(405).end();
   }
-  if (req.method === "POST") {
-    const appliedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
-    const hasAppliedMigrations = appliedMigrations.length;
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+    const defaultMigrationOptions = {
+      dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+    if (req.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      return res.status(200).json(pendingMigrations);
+    }
+    if (req.method === "POST") {
+      const appliedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+      const hasAppliedMigrations = appliedMigrations.length;
+      return res
+        .status(hasAppliedMigrations ? 201 : 200)
+        .json(appliedMigrations);
+    }
+  } catch (error) {
+    return res.status(500).end();
+  } finally {
     await dbClient.end();
-    return res.status(hasAppliedMigrations ? 201 : 200).json(appliedMigrations);
   }
-  await dbClient.end();
-  return res.status(405).end();
 }
