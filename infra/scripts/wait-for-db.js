7f184092834741b5572d5cde66a98a62e2f94106
GustavoMont <lgmluisgm@gmail.com>
2024-07-12 08:32:14 -0300

    fixes code lint problems

--- infra/scripts/wait-for-db.js
+++ infra/scripts/wait-for-db.js
@@ -2,7 +2,7 @@ const { exec } = require("node:child_process");
 function checkDb() {
   exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
 
-  function handleReturn(err, stdout, stderr) {
+  function handleReturn(err, stdout) {
     if (stdout.search("accepting connections") === -1) {
       process.stdout.write(".");
       checkDb();
