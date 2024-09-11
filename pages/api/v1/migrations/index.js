93f8ca63f7dc18cc2fd2d2bf4cdfecc6f9c64106
GustavoMont <lgmluisgm@gmail.com>
2024-09-11 08:49:03 -0300

    fix: update migrations path at `/api/v1/migrations` endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,5 +1,5 @@
 import migrationRunner from "node-pg-migrate";
-import { join } from "node:path";
+import { resolve } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(req, res) {
@@ -13,7 +13,7 @@ export default async function migrations(req, res) {
     const defaultMigrationOptions = {
       dbClient,
       dryRun: true,
-      dir: join("infra", "migrations"),
+      dir: resolve("infra", "migrations"),
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
