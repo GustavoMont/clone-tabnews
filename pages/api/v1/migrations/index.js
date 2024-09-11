13265d737135b931228986dc5bfc578ba9e77643
GustavoMont <lgmluisgm@gmail.com>
2024-09-11 08:43:19 -0300

    chore: add a error log output in `/api/v1/migrations` endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -33,6 +33,7 @@ export default async function migrations(req, res) {
         .json(appliedMigrations);
     }
   } catch (error) {
+    console.error(error);
     return res.status(500).end();
   } finally {
     await dbClient.end();
