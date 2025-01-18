9aef733f4741b1a2e1cca1eb864523cf683e8eee
GustavoMont <lgmluisgm@gmail.com>
2025-01-18 11:13:20 -0300

    feat(infra): create `MethodNotAllowedError` custom error

--- infra/errors.js
+++ infra/errors.js
@@ -15,3 +15,22 @@ export class InternalServerError extends Error {
     };
   }
 }
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Método não permitido para este endpoint.");
+    this.statusCode = 405;
+    this.action =
+      "Verifique se o método HTTP enviado é válido para este endpoint.";
+    this.name = "MethodNotAllowedError";
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
