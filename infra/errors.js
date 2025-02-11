5c675aac728979469379c995be8c2f6aab7e35e0
GustavoMont <lgmluisgm@gmail.com>
2025-02-10 21:06:38 -0300

    feat: create `ServiceError`

--- infra/errors.js
+++ infra/errors.js
@@ -1,9 +1,27 @@
 export class InternalServerError extends Error {
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super("Um erro interno não esperado aconteceu", { cause });
-    this.statusCode = 500;
+    this.statusCode = statusCode || 500;
     this.action = "Entre em contato com o suporte";
     this.name = "InternalServerError";
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ message, cause }) {
+    super(message || "Serviço indisponível no momento", { cause });
+    this.statusCode = 503;
+    this.action = "Verifique se o serviço está disponível";
+    this.name = "ServiceError";
   }
 
   toJSON() {
